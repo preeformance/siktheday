659cd488219a97a8202c61b4e1b06a126df5ecb2
Pree <siktheday@gmail.com>
2022-09-12 14:19:53 -0400

    updated model to match form

--- models/Place.js
+++ models/Place.js
@@ -1,13 +1,42 @@
 const mongoose = require('mongoose')
 
 const PlaceSchema = new mongoose.Schema({
-  place: {
+  placeDate: {
+    type: Date,
+    required : true,
+    default: Date.now
+  },
+  placeItem: {
     type: String,
     required: true,
+  },
+  placeLocation: {
+    type: String,
+    required: false,
+  },
+  placeAuthenticRating: {
+    type: Number,
+    required: true,
+  },
+  placeAtmosphereRating: {
+    type: Number,
+    required: false,
+  },
+  placeTasteRating: {
+    type: Number,
+    required: false,
   },
   visited: {
     type: Boolean,
     required: true,
+  },
+    favoritePlace: {
+    type: Boolean,
+    required:false,
+  },
+    placeNotes: {
+    type: String,
+    required: false,
   },
   userId: {
     type: String,
