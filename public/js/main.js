f2ec608950ba4f972ead594903ad84645999eb56
Pree <siktheday@gmail.com>
2022-09-29 12:28:51 -0400

    increased accessibility

--- public/js/main.js
+++ public/js/main.js
@@ -10,9 +10,9 @@ Array.from(placeItem).forEach((el)=>{
     el.addEventListener('click', markVisited)
 })
 
-Array.from(placeComplete).forEach((el)=>{
-    el.addEventListener('click', marktoVisit)
-})
+// Array.from(placeComplete).forEach((el)=>{
+//     el.addEventListener('click', marktoVisit)
+// })
 
 async function deletePlace(){
     const placeId = this.parentNode.dataset.id
@@ -32,23 +32,23 @@ async function deletePlace(){
     }
 }
 
-async function markComplete(){
-    const placeId = this.parentNode.dataset.id
-    try{
-        const response = await fetch('places/markVisited', {
-            method: 'put',
-            headers: {'Content-type': 'application/json'},
-            body: JSON.stringify({
-                'placeIdFromJSFile': placeId
-            })
-        })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
-    }catch(err){
-        console.log(err)
-    }
-}
+// async function markComplete(){
+//     const placeId = this.parentNode.dataset.id
+//     try{
+//         const response = await fetch('places/markVisited', {
+//             method: 'put',
+//             headers: {'Content-type': 'application/json'},
+//             body: JSON.stringify({
+//                 'placeIdFromJSFile': placeId
+//             })
+//         })
+//         const data = await response.json()
+//         console.log(data)
+//         location.reload()
+//     }catch(err){
+//         console.log(err)
+//     }
+// }
 
 async function marktoVisit(){
     const placeId = this.parentNode.dataset.id
